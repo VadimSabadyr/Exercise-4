35e3d217dbe898e4d225c35b7690c0f9369d8971
VadimSabadyr <105793141+VadimSabadyr@users.noreply.github.com>
2022-06-07 12:45:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,32 @@
 class Beverage:
-
     prices = {"Strawberries": 1.5, "Banana": 0.5, "Mango": 2.5,
               "Blueberries": 1, "Raspberries": 1, "Apple": 1.75,
               "Pineapple": 3.5}
-    def __init__(self, ingredients, c = 0.00, cout = 0):
-        self.ingredients = ingredients
-        self.c = c
-        self.cout = cout
+    cout = 0
 
+    def __init__(self, ingredients):
+        self.ingredients = ingredients
 
     def get_cost(self):
         for i in self.ingredients:
             for k, v in self.prices.items():
                 if i == k:
-                    self.c += v
-        x = self.c
-        return f"${self.c:.2f}"
+                    self.cout += v
+        return f"${self.cout:.2f}"
 
     def get_price(self):
-        return f"${self.c * 2.5:.2f}"
-
+        return f"${self.cout * 2.5:.2f}"
 
     def get_name(self):
+        cout = 0
         x = []
-        self.ingredients = [name.replace("berries", "berry") for name in self.ingredients]
-        name = self.ingredients.sort()
-        for i in self.ingredients:
+        ingred = [name.replace("berries", "berry") for name in self.ingredients]
+        ingred.sort()
+        for i in ingred:
             x.append(i)
-            self.cout += 1
+            cout += 1
         st = " ".join([str(item) for item in x])
-        if self.cout == 1:
+        if cout == 1:
             return f"{st} Smoothie"
-        elif self.cout > 1:
-            return (f"{st} Fusion")
+        elif cout > 1:
+            return f"{st} Fusion"
